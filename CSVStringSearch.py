fd7d925907a329d4c25d26f3496160d52f35b022
borchr27 <30349201+borchr27@users.noreply.github.com>
2019-05-08 15:07:19 -0400

    Create CSVStringSearch.py

---
+++ CSVStringSearch.py
@@ -0,0 +1,31 @@
+"""
+    Getting values out of CSV file
+    Input:
+        keys- list of strings to match lines with 
+        csv_file - file with lines to match
+    Output:
+        valid_lines - lines which contain all keywords provided
+        lines - list of all lines in file
+"""
+def keyword_search(keys, csv_file):
+    keys_lst = keys.lower().split()
+    # Only take the description part of the line (splitting on the first comma)
+    lines = [line.split(',')[0] for line in open(csv_file, 'r')]
+    # list of counters for number of keywords found in each line
+    cnts = [0 for i in range(len(lines))]
+    for k in keys_lst:
+        # loop through all the keys
+        for i in range(len(lines)):
+            # check if the key is in the i'th line
+            if k in lines[i]:
+                cnts[i] += 1 # update count if key is found
+    # get index of lines which contain all the keys
+    max_idx = [i for i, j in enumerate(cnts) if (j == len(keys_lst))]
+    valid_lines = [lines[i] for i in max_idx]
+    return(valid_lines, lines)
+     
+# Running an example 
+valid_lines , _ = keyword_search('robotic aluminum', 'CMC cycle time data.csv')
+print(valid_lines)
+valid_lines , _ = keyword_search('robotic weld', 'CMC cycle time data.csv')
+print(valid_lines)
